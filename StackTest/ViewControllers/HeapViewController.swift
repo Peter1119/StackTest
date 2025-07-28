1875b8c011f8f306f63e1e67c523c66fe383fb25
홍석현 <ghdtjrgus333@naver.com>
2025-07-28 17:28:43 +0900

    feat: Add .gitignore and clean up tracked files

---
+++ StackTest/ViewControllers/HeapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HeapViewController.swift
+//  StackTest
+//
+//  Created by 홍석현 on 7/28/25.
+//
+
+import UIKit
+
+class HeapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
